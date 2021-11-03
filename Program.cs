8fc2b597522c4f14d7ee1283943bb1de97e31c48
antjamespadgett <padgettanthonyj@gmail.com>
2021-11-03 13:33:26 -0500

    practiced simplemadlib

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace BuildingMadLib
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //string color, pluralNoun, celebrity;
+
+
+            //Console.Write("Enter a color: ");
+            //color = Console.ReadLine();
+
+            //Console.Write("Enter a plural Noun: ");
+            //pluralNoun = Console.ReadLine();
+
+            //Console.Write("Enter a celebrity: ");
+            //celebrity = Console.ReadLine();
+
+
+            //Console.WriteLine($"Rose are {color}");
+            //Console.WriteLine($"{pluralNoun} are blue");
+            //Console.WriteLine($" I Love {celebrity}");
+
+            //Console.ReadLine();
+
+
+            //string color, pluralNoun, celebrity;
+
+            //Console.Write("Enter your favorite color: ");
+            //color = Console.ReadLine();
+
+            //Console.Write("Enter your favorite plural noun: ");
+            //pluralNoun = Console.ReadLine();
+
+            //Console.Write("Enter your favorite celebrity: ");
+            //celebrity = Console.ReadLine();
+
+            //Console.WriteLine($"Roses are {color}");
+            //Console.WriteLine($"{pluralNoun} are green");
+            //Console.WriteLine($"{celebrity} is so mean!!");
+
+            //Console.ReadLine();
+
+
+
+            //string color, celebrity, pluralNoun;
+
+            //Console.Write("What is your favorite color: ");
+            //color = Console.ReadLine();
+            //Console.Write("What is your favoite pluralNoun: ");
+            //pluralNoun = Console.ReadLine();
+            //Console.Write("What is your favorite celebrity: ");
+            //celebrity = Console.ReadLine();
+
+            //Console.WriteLine($"Roses are {color}");
+            //Console.WriteLine($"{pluralNoun} are green");
+            //Console.WriteLine($"{celebrity} is ohhhh so mean!!" );
+
+            //Console.ReadLine();
+
+
+            //Console.Write("Enter your favorite number: ");
+            //double num1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Enter another number: ");
+            //double  num2= Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine(num1 *num2);
+            //Console.ReadLine();
+
+            Console.Write("enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine(num1*num2);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
