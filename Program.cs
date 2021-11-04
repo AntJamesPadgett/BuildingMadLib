0f10397e5c6ea2b193a0732c9d095b6bc15920e8
antjamespadgett <padgettanthonyj@gmail.com>
2021-11-04 13:48:42 -0500

    added a few more things

--- Program.cs
+++ Program.cs
@@ -79,20 +79,76 @@ namespace BuildingMadLib
 
             //Console.ReadLine();
 
-            string color, pluralNoun, celebrity;
+            //string color, pluralNoun, celebrity;
 
-            Console.Write("Enter your favorite color:");
-            color = Console.ReadLine();
-            Console.Write("Enter your favorite plural noun: ");
-            pluralNoun = Console.ReadLine();
-            Console.Write("Enter your favorite celebrity: ");
-            celebrity = Console.ReadLine();
+            //Console.Write("Enter your favorite color:");
+            //color = Console.ReadLine();
+            //Console.Write("Enter your favorite plural noun: ");
+            //pluralNoun = Console.ReadLine();
+            //Console.Write("Enter your favorite celebrity: ");
+            //celebrity = Console.ReadLine();
 
-            Console.WriteLine($"Roses are {color}");
-            Console.WriteLine($"{pluralNoun} are green");
-            Console.WriteLine($"{celebrity} is soooo mean");
+            //Console.WriteLine($"Roses are {color}");
+            //Console.WriteLine($"{pluralNoun} are green");
+            //Console.WriteLine($"{celebrity} is soooo mean");
 
+            //Console.ReadLine();
+
+            //Console.Write("enter a number :");
+            //double num1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter another number: ");
+            //double num2 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine(num1*num2);
+
+            //Console.ReadLine();
+
+            //string color, pluralNoun, celebrity;
+
+            //Console.Write("Enter your favorite color: ");
+            //color = Console.ReadLine();
+            //Console.Write("Enter a pluralnoun: ");
+            //pluralNoun = Console.ReadLine();
+            //Console.Write("Enter a favorite celebrity: ");
+            //celebrity = Console.ReadLine();
+
+            //Console.WriteLine($"Roses are {color} ");
+            //Console.WriteLine($"{pluralNoun} are green");
+            //Console.WriteLine($"{celebrity} is a Jackass and very mean!!!!");
+
+            //string[] pizza = new string[4];
+            //pizza[0] = "Extra Cheese";
+            //pizza[1] = "Ham";
+            //pizza[2] = "Pineapple";
+            //pizza[3] = "Chickem";
+
+            //Console.WriteLine(pizza[1]);
+            //Console.ReadLine();
+
+            //string[] Baseball = new string[2] { "Yankees", "Mets" };
+
+            //Console.WriteLine(Baseball[1]);
+            //Console.ReadLine();
+
+            //double[] basketball = new double[3] { 32.4, 22.7, 19.5 };
+            //basketball[0] = 32.4;
+            //basketball[1] = 22.7;
+            //basketball[2] = 25.5;
+
+            //Console.WriteLine(basketball[2]);
+            //Console.ReadLine();
+
+
+            string[] pizza = new string[4];
+            pizza[0] = "extra cheese";
+            pizza[1] = "ground beef";
+            pizza[2] = "chicken";
+            pizza[3] = "Pepperoni";
+
+            pizza[2] = "double cheese";
+            Console.WriteLine(pizza[2]);
             Console.ReadLine();
+
 
 
 
